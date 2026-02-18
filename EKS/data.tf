ef911c0dfce689960866e42cd293b039d4feb2bf
Devssmat <syjusacademy@gmail.com>
2026-02-18 10:05:04 +0530

    pro6

--- EKS/data.tf
+++ EKS/data.tf
@@ -1,11 +1,1 @@
-data "aws_eks_cluster" "eks-cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "eks-cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_caller_identity" "current" {}
-
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "azs" {}
